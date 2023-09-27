fac3b83dcd81f2d8825f878b4962a3a805305e5a
Alejo Fariña <scaramouch11@gmail.com>
2023-09-27 13:25:26 -0300

    add carrousel - import img

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -1,16 +1,16 @@
 import style from './Projects.module.css';
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-// import image1 from '../../assets/img/home/homeImg.png';
-// import image2 from '../../assets/img/home/homeImg2.png';
-// import image3 from '../../assets/img/home/homeImg3.png';
-// import Card from './Card/Card';
+import image1 from '../../assets/img/imgHeliki1.png';
+import image2 from '../../assets/img/imgHeliki2.png';
+import image3 from '../../assets/img/imgHeliki3.png';
+import image4 from '../../assets/img/imgHeliki4.png';
+import image5 from '../../assets/img/imgHeliki5.png';
 
 const Projects = () => {
     const [selectedTitle, setSelectedTitle] = useState("WWII Comfort Women");
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    // const images = [image1, image2, image3];
+    const images = [image1, image2, image3, image4, image5];
 
     const textData = [
         {
@@ -19,8 +19,8 @@ const Projects = () => {
                 <>
                     <p>
                         "Mujeres de confort" (Comfort Women) for the Manyoung Foundation (Arg.) is an SPA using PERN stack. It is an informational website about the Korean comfort women in the WWII.
+                        WIP
                     </p>
-                    <p>WIP</p>
                     <p><a href="https://github.com/AAlejof/WWII-ComfortWomen" target="_blank">REPOSITORY</a></p>
                 </>
             ),
@@ -33,22 +33,26 @@ const Projects = () => {
             title: "Wedding RSVP app",
             summary: (
                 <>
-                    <p>This is an SPA using MERN stack. It Allows guest to RSVP for a wedding and to the admin to have control of those attending and their menu or allergies needs</p>
+                    <p>This is an SPA using MERN stack. It Allows guest to RSVP for a wedding and access important info about the ceremony, party and city hosting it. Also the admin have control of those attending and their menu or allergies needs</p>
+                    <p><a href="https://github.com/AAlejof/HeliKi" target="_blank">REPOSITORY</a></p>
+                    <p><a href="http://helen.ricardokim.com.ar/" target="_blank">DEPLOY</a></p>
+
                 </>
             ),
             img: (
                 <>
-                    <p>Las mujeres esclavizadas sexualmente pertenecieron a los distintos territorios ocupados por Japón durante la guerra. Además, de mujeres de sus dos colonias: Taiwán y Corea, mujeres de su propio país, y mujeres holandesas, hijas de los colonos en de las Indias Orientales Neerlandesas. Es decir, hubo víctimas coreanas, chinas, japonesas, filipinas, vietnamitas, camboyanas, laosianas, birmanas, malayas, indonesias, holandesas, tailandesas, timorenses, papú neoguineanos.</p>
-                    <p>Se calcula que entre 200.000 y 400.000 mujeres fueron llevadas en contra de su voluntad a las “estaciones de confort”. Este sistema de trata de personas es el más grande que haya existido en el marco de un conflicto armado contemporáneo. </p>
+                    <div className={style.carouselContainer}>
+                        <img className={style.descImg} src={images[currentImageIndex]} alt="Mujeres de confort" />
+                    </div>
                 </>)
         },
         {
             title: "Complex gym",
             summary: (
                 <>
-                    <p>A partir de la invasión de Manchuria en 1931, el Imperio japonés inicia una etapa de avance militar en Asia que finalizará con el discurso de rendición del emperador de japón, Hirohito, el 14 de agosto de 1945.</p>
-                    <p>(...)</p>
-                    <p>Se han encontrado diversos documentos que prueban la existencia de este sistema de esclavización sexual. </p>
+                    <p>Fully responsive SPA made with PERN stack, Auth0, Redux, Sequelize, Tailwind, MercadoPago among others. It allows users to suscribe to the gym, choose plans and make payments. Also, the gym owner can fully manage all the functions from the admin dashboard and have control of clients, payments, blogs, posts and activities</p>
+                    <p><a href="https://github.com/AAlejof/PF-Complex-Gym-JS" target="_blank">REPOSITORY</a></p>
+                    <p><a href="https://www.youtube.com/watch?v=_ncVIXZZmH0&ab_channel=ComplexGym" target="_blank">VIDEO</a></p>
                 </>
             ),
             img: (
@@ -61,48 +65,42 @@ const Projects = () => {
 
     ];
 
-    // const nextImage = () => {
-    //     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    // };
+    const nextImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
 
-    // useEffect(() => {
-    //     const interval = setInterval(nextImage, 5000);
+    useEffect(() => {
+        const interval = setInterval(nextImage, 5000);
 
-    //     return () => clearInterval(interval);
-    // }, []);
+        return () => clearInterval(interval);
+    }, []);
 
-    // const prevImage = () => {
-    //     setCurrentImageIndex((prevIndex) =>
-    //         prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    //     );
-    // };
+    const prevImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
 
-    // const nextManualImage = () => {
-    //     nextImage();
-    // };
+    const nextManualImage = () => {
+        nextImage();
+    };
 
-    // const prevManualImage = () => {
-    //     prevImage();
-    // };
+    const prevManualImage = () => {
+        prevImage();
+    };
 
-                        {/* <div className={style.carouselContainer}>
-                        <div className={style.carouselButtons}>
-                            <button onClick={prevManualImage} className={style.chevron}>&#8249;</button>
-                            <button onClick={nextManualImage} className={style.chevron}>&#8250;</button>
-                        </div>
-                        <img className={style.descImg} src={images[currentImageIndex]} alt="Mujeres de confort" />
-                    </div> */}
+
 
     return (
         <>
             <div>
                 <div className={style.infoDiv}>
-                <div className={style.mainDiv}>
-                    <div className={style.descDiv}>
-                        <h2 className={style.descH2}>Projects</h2>
-                     </div>
+                    <div className={style.mainDiv}>
+                        <div className={style.descDiv}>
+                            <h2 className={style.descH2}>Projects</h2>
+                        </div>
 
-                </div>
+                    </div>
                     <div className={style.titleDiv}>
                         {textData.map(item => (
                             <>
