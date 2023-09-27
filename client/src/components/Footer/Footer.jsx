b2555a0427933db260f01e97859ca78880226cb4
Alejo Fariña <scaramouch11@gmail.com>
2023-09-27 12:29:27 -0300

    add email to footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
         <>
             <div className={style.footerDiv}>
                 <p className={style.pFooterDiv}>AALEJOF · 2023</p>
+                <p className={style.pFooterDiv}><a href="mailto:adrianalejof@gmail.com">CONTACT ME</a></p>
             </div>
         </>
     )
